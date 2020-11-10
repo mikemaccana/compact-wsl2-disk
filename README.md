04f55f47ac3d587650fed2ceada456ba2add99f0
Mike MacCana <mike.maccana@gmail.com>
2020-11-10 17:26:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# WSL2 Compacter
+
+## A small script for Windows 10 Home users to compact their WSL2 disks
+
+Inspired by this post on [GitHub](https://github.com/microsoft/WSL/issues/4699#issuecomment-627133168).
+
+## Warning
+
+1. I am a strange man on the internet asking you to run a powershell script. You shpuld probably look inside the script `compact-wsl2-disk.ps1` and make sure you understand what it does.
+
+2. This will stop WSL. So save your work in Linux.
+
+## Usage
+
+`powershell.exe .\compact-wsl2-disk.ps1`
+
+## PRs are welcome!
+
+Please file tickets instead of just asking forthings. Thanks!
+
+## Thanks
+
+[This person on StackOverflow](https://stackoverflow.com/questions/64772243/can-diskpart-take-command-line-parameters-or-can-i-fake-them-with-powershell)
---
+++ compact-wsl2-disk.ps1
@@ -0,0 +1,37 @@
+#C:\WSL-Distros\…\ext4.vhdx
+
+$ErrorActionPreference = "Stop"
+
+# File is normally under something like C:\Users\onoma\AppData\Local\Packages\CanonicalGroupLimited...
+$files = @()
+cd $env:LOCALAPPDATA\Packages
+get-childitem -recurse -filter "ext4.vhdx" -ErrorAction SilentlyContinue | foreach-object {
+  $files += ${PSItem}
+}
+
+if ( $files.count -gt 1 ) {
+  throw "We found too many files in $env:LOCALAPPDATA\Packages"
+}
+
+if ( $files.count -eq 0 ) {
+  throw "We could not find a file called ext4.vhdx in $env:LOCALAPPDATA\Packages"
+}
+
+$disk = $files[0].FullName
+
+
+write-output " - Successfully found VHDX file $disk"
+write-output " - Shutting down WSL2"
+wsl --shutdown
+write-output " - Compacting disk (starting diskpart)"
+
+@"
+select vdisk file=$disk
+attach vdisk readonly
+compact vdisk
+detach vdisk
+exit
+"@ | diskpart
+
+write-output ""
+write-output "Success. Compacted $disk."
